ab60aae681f01dfdb7d1e60b220955aad7e8df4c
Quin Carter <quincarter@macbook-pro.lan>
2019-06-12 09:10:49 -0500

    Adding more

--- computerSetup.sh
+++ computerSetup.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 # Installs and sets up apps for macOs Dev setup
 
+currentUser=`ls -l /dev/console | cut -d " " -f4`
+
+sudo -u $currentUser defaults write com.apple.menuextra.battery ShowPercent YES
+
+sudo -u $currentUser killall SystemUIServer
+
+git clone https://github.com/kerma/defaultbrowser.git ~/Downloads/defaultbrowser && cd ~/Downloads/defaultbrowser && make && make install && cd ~/computerSetup.sh
+
+
 #Mac App Store Apps First
 mas install 803453959  # Slack
 
@@ -10,10 +19,10 @@ xcode-select --install
 echo Install Homebrew packages
 brew install wget
 brew install mackup
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 brew install bash-completion
-brew tap homebrew/completions
-brew install homebrew/completions/packer-completion
 
 brew tap phinze/cask
 brew install brew-cask
@@ -36,19 +45,27 @@ brew cask install spotify
 
 echo "Installing Google Chrome"
 brew cask install google-chrome
+defaultbrowser chrome 
+rm -rf defaultbrowser
 
 # Install docker
-open https://www.docker.com/products/docker\#/mac
+brew cask install docker       # Install Docker
 
 # Install Jetbrains Toolbox
-open https://www.jetbrains.com/toolbox/app/
+brew cask install jetbrains-toolbox
 
 # cleanup
-brew cleanup --force
-rm -f -r /Library/Caches/Homebrew/*
+brew cleanup --prune
 
-# restore dot files
-mackup restore
+echo Install DockUtil and run ./dock_setup.sh
+open https://github.com/kcrawford/dockutil/downloads
 
-# mac defaults
-defaults write com.apple.screencapture type jpg
+cd Downloads
+curl -O https://raw.githubusercontent.com/MartinSeeler/iterm2-material-design/master/material-design-colors.itermcolors
+
+echo Go to iTerm2 > Preferences > Profiles > Colors Tab
+echo Click Color Presets… at the bottom right
+echo Click Import…
+echo Select the material-design-colors.itermcolors file
+echo Select the material-design-colors from Load Presets…
+
---
+++ dockSetup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+dockutil --add /Applications/Docker.app
+dockutil --add /Applications/iTerm.app
+dockutil --add /Applications/Google\ Chrome.app
+dockutil --add /Applications/Jetbrains\ Toolbox.app
+dockutil --add /Applications/Spotify.app
+dockutil --add /Applications/Spectacle.app
+dockutil --add /Applications/Sourcetree.app
+dockutil --add /Applications/VirtualBox.app
+
+# Removing unneccessary default items
+dockutil --remove Safari
+dockutil --remove Books
+dockutil --remove Calculator
+dockutil --remove Contacts
+dockutil --remove Calendar
+dockutil --remove Mission Control
+dockutil --remove Mail
+dockutil --remove News
+dockutil --remove Photo Booth
+dockutil --remove Photos
+dockutil --remove Reminders
+dockutil --remove Stickies
+dockutil --remove Time Machine
+dockutil --remove Voice Memos
+dockutil --remove Messages
+dockutil --remove Maps
+dockutil --remove iTunes
+dockutil --remove FaceTime
+
+killall Dock
